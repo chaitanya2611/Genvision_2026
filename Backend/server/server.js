6d471ff4a42c2c250bdca066c087046afceed809
chaitanya2611 <115576523+chaitanya2611@users.noreply.github.com>
2025-10-19 01:57:36 +0530

    13th commit

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -41,6 +41,6 @@ mongoose.connect(process.env.MONGO_URI)
   const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../Frontend/build/index.html"));
 });
