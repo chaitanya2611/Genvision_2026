5dfc62c54e944e0338ebb495c9e0b238cabaabb1
chaitanya2611 <115576523+chaitanya2611@users.noreply.github.com>
2025-10-19 01:30:02 +0530

    10th commit

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -2,6 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve frontend build
+
+
+
 
 import eventRoutes from "./routes/events.js";
 import participantRoutes from "./routes/participants.js";
@@ -13,6 +23,7 @@ import aboutRouter from "./routes/about.js";
 dotenv.config();
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../Frontend/build")));
 app.use(cors());
 app.use(express.json());
 app.use("/api/events", eventRoutes);
@@ -27,4 +38,9 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.log(err));
 
-app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+  const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log("🚀 Server running on port "));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend/build/index.html"));
+});
